517369d037c3449caaa0526632c13036c9f794d9
DANU011 <122770856+DANU011@users.noreply.github.com>
2023-05-13 16:28:42 +0900

    0513

--- DA/2주차 공동과제/flask-scikit/model.py
+++ DA/2주차 공동과제/flask-scikit/model.py
@@ -1,44 +1,56 @@
 import pandas as pd
 import numpy as np
-
-url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
-dataset = pd.read_csv(url)
-
-X = dataset[['Sex', 'Age', 'Pclass']].values
-y = dataset['Survived'].values
-print(X)
-
 from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer.fit(X[:, [1]])
-X[:, [1]] = imputer.transform(X[:, [1]])
-
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
 from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
 from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
-classifier.fit(X_train, y_train)
-
-from sklearn.metrics import confusion_matrix, accuracy_score
-y_pred = classifier.predict(X_test)
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-print(accuracy_score(y_test, y_pred))
-
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 import joblib
-joblib.dump(classifier, './model/model.pkl')
-print("Model sucesfully dumped")
 
-joblib.dump(sc, './model/sc.pkl')
-joblib.dump(ct, './model/ct.pkl')
+# 데이터 불러오기
+train_df = pd.read_csv('./data/train.csv')
+test_df = pd.read_csv('./data/test.csv')
+
+# 데이터 분할
+X_train = train_df.iloc[:, :-2].values
+y_train = train_df.iloc[:, -1].values
+X_test = test_df.iloc[:, :-2].values
+y_test = test_df.iloc[:, -1].values
+
+# 결측치 처리
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X_train)
+X_train = imputer.transform(X_train)
+X_test = imputer.transform(X_test)
+
+# 표준화
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# 독립변수, 종속변수 설정 -> 독립변수 : 가속도계, 자이로스코프 데이터 / 종속변수 : 활동 레이블
+X = X_train
+Y = y_train
+
+# 로지스틱 회귀 모델 생성
+model = LogisticRegression(max_iter=3000)
+model.fit(X, Y)
+
+# 모델 평가
+y_pred = model.predict(X_test)
+
+def get_clf_eval(test, pred):
+    confusion = confusion_matrix(test, pred)
+    accuracy = accuracy_score(test, pred)
+    precision = precision_score(test, pred, average='weighted')
+    recall = recall_score(test, pred, average='weighted')
+    f1 = 2 * (precision * recall) / (precision + recall)
+    print('오차 행렬')
+    print(confusion)
+    print('Accuracy:', accuracy, 'precision:', precision, 'recall:', recall, 'f1:', f1)
+
+get_clf_eval(y_test, y_pred)
+
+# 모델과 전처리 객체를 저장
+joblib.dump(model, 'model.pkl')
+joblib.dump(scaler, 'scaler.pkl')
+joblib.dump(imputer, 'imputer.pkl')
