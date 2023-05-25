1e690fd83f6c8e6595364d9a36d1c27dd39b6c83
DANU011 <122770856+DANU011@users.noreply.github.com>
2023-05-25 10:34:32 +0900

    rd 수정

--- DA/5주차/app.py
+++ DA/5주차/app.py
@@ -167,9 +167,9 @@
 #=================================================================================================#
 #=================================================================================================#
 
-import time
+# import time
 import csv
-from flask import Flask, request, json, Response
+from flask import Flask, request, json #, Response
 import numpy as np
 import joblib
 
@@ -252,6 +252,105 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+#=================================================================================================#
+#=================================================================================================#
+#=================================================================================================#
+#=================================================================================================#
+
+
+# import time
+# import csv
+# from flask import Flask, request, json, Response
+# import numpy as np
+# import joblib
+#
+# app = Flask(__name__)
+#
+# # KNN 모델 경로 지정
+# knn_model_file = "./model/model_knn.pkl"
+# scaler_file = "./model/scaler_knn.pkl"
+#
+# # KNN 모델 로드
+# knn_model = joblib.load(knn_model_file)
+#
+# # Scaler 로드
+# scaler = joblib.load(scaler_file)
+#
+# # 필드 인덱스 추출
+# def extract_field_indexes(header):
+#     temperature_idx = header.index("Temperature")
+#     heartbeat_idx = header.index("Heartbeat")
+#     gyrox_idx = header.index("GyroX")
+#     gyroy_idx = header.index("GyroY")
+#     gyroz_idx = header.index("GyroZ")
+#     return temperature_idx, heartbeat_idx, gyrox_idx, gyroy_idx, gyroz_idx
+#
+# # 데이터 전처리 함수
+# def preprocess_data(data):
+#     # 데이터 타입을 float로 변환
+#     selected_data = [float(value) for value in data]
+#     scaled_data = scaler.transform(np.array(selected_data).reshape(1, -1))
+#     return scaled_data
+#
+#
+# # CSV 파일 레코드 한 줄씩 처리 함수
+# def process_record(record, temperature_idx, heartbeat_idx, gyrox_idx, gyroy_idx, gyroz_idx):
+#     # 선택한 필드 추출
+#     selected_data = [record[temperature_idx], record[heartbeat_idx], record[gyrox_idx], record[gyroy_idx], record[gyroz_idx]]
+#
+#     # 데이터 전처리
+#     preprocessed_data = preprocess_data(selected_data)
+#
+#     # 예측
+#     prediction = knn_model.predict(preprocessed_data)
+#
+#     # 예측 결과 반환
+#     return prediction.tolist()  # 리스트로 변환하여 반환
+#
+#
+# # /predict 엔드포인트
+# @app.route("/predict", methods=["POST"])
+# def predict():
+#     file = request.files.get("file")
+#
+#     if not file:
+#         return "No file provided"
+#
+#     if file.filename == "":
+#         return "No file selected"
+#
+#     # CSV 파일 열기
+#     csv_reader = csv.reader(file.read().decode('utf-8').splitlines())
+#
+#     # 첫 줄은 헤더로 처리
+#     header = next(csv_reader)
+#
+#     # 필드 인덱스 추출
+#     temperature_idx, heartbeat_idx, gyrox_idx, gyroy_idx, gyroz_idx = extract_field_indexes(header)
+#
+#     # 예측 결과 리스트 초기화
+#     predictions = []
+#
+#     # 각 레코드를 한 줄씩 읽어 처리
+#     for record in csv_reader:
+#         # 레코드 처리
+#         prediction = process_record(record, temperature_idx, heartbeat_idx, gyrox_idx, gyroy_idx, gyroz_idx)
+#         predictions.append(prediction)
+#
+#     # 예측 결과를 스트리밍으로 반환
+#     def predictions_stream():
+#         for prediction in predictions:
+#             yield json.dumps({"prediction": prediction}) + "\n"
+#
+#     return Response(predictions_stream(), mimetype="application/json")
+#
+#
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
 
 
 
