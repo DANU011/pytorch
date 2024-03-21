af4b9ab3bb46c80c9556c1f2999c2af116a1af24
DANU011 <122770856+DANU011@users.noreply.github.com>
2024-03-21 15:33:23 +0900

    init

--- DA/3주차/tensor install.py
+++
@@ -1,8 +0,0 @@
-# import tensorflow as tf
-# print(tf.__version__)
-#
-# from tensorflow.python.client import device_lib
-# print(device_lib.list_local_devices())
-
-import tensorflow as tf
-tf.config.list_physical_devices('GPU')
